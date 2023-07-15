5822d0b6bac15b0e224698d1945b1a15f0dfcd39
sachindolase <sachindolase@gmail.com>
2023-07-15 12:23:38 +0530

    restaurant-menu

---
+++ src/component/Basics/menuApi.js
@@ -0,0 +1,86 @@
+const Menu = [
+    {
+      id: 1,
+      image: "images/maggi.jpg",
+      name: "maggi",
+      category: "breakfast",
+      price: "12₹",
+      description:
+        "I love Maggi realy oo yues  Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto. Accusamus quas, soluta ipsam autem eius necessitatibus fugiat in . ",
+    },
+  
+    {
+      id: 2,
+      image: "images/allupakoida.jpg",
+      name: "allu pakoida",
+      category: "evening",
+      price: "20₹",
+      description:
+        "Aalo pakoda is a popular Indian snack made from sliced potatoes coated in a seasoned chickpea flour batter and deep-fried until crispy and golden brown. The sliced potatoes are mixed with spices such as chili powder, turmeric, cumin, and salt to add flavor and then dipped in a batter made from chickpea flour (besan) mixed with water and various spices. The batter-coated potato slices are then deep-fried until they become crispy on the outside and soft on the inside. Aalo pakoda is often served hot with chutneys or sauces as a delicious and savory snack. It is a favorite street food in India and is enjoyed by people of all ages."
+      },
+    {
+      id: 3,
+      image: "images/corn.jpg",
+      name: "corn",
+      category: "breakfast",
+      price: "10₹",
+      description:
+        "Popcorn is a popular snack made from dried corn kernels that puff up when heated. It is typically prepared by placing the kernels in a pot or popcorn machine with a little oil, then heating it over high heat. As the heat increases, the water inside the kernels turns into steam, creating pressure within the kernel. Eventually, the pressure becomes too much for the outer shell, causing it to burst open, turning the kernel inside out and forming a fluffy, edible snack. Popcorn is often enjoyed plain or seasoned with salt, butter, or various flavors like caramel or cheese. It is a popular choice for movie nights, parties, and a quick and satisfying snack option."
+      },
+    {
+      id: 4,
+      image: "../images/chola.jpg",
+      name: "chola",
+      category: "lunch",
+      price: "50₹",
+      description:
+        "Chole kulche is a popular North Indian street food dish consisting of spicy chickpea curry (chole) served with soft, fluffy bread (kulche). The chickpea curry is made by simmering cooked chickpeas in a flavorful tomato-based gravy, seasoned with a blend of spices such as garam masala, cumin, coriander, and chili powder. The curry is typically rich and tangy, with a hint of spice. The kulche, which is a type of leavened bread, is usually made with all-purpose flour and yeast, giving it a light and fluffy texture. It is often topped with sesame seeds or nigella seeds for added flavor. Chole kulche is commonly enjoyed as a hearty and satisfying meal, often accompanied by pickles, onions, and yogurt on the side."
+       },
+    {
+      id: 5,
+      image: "../images/pizza.jpg",
+      name: "pizza",
+      category: "evening",
+      price: "80₹",
+      description:
+        "Pizza is a popular Italian dish that has become a global favorite. It typically consists of a round, flatbread crust topped with a variety of ingredients. The crust is usually made from a mixture of flour, water, yeast, and salt, which is rolled out into a thin circle and baked until crispy. The toppings can vary widely but often include tomato sauce, cheese (such as mozzarella), and various ingredients like pepperoni, mushrooms, onions, bell peppers, olives, or other meats and vegetables. After assembling the toppings, the pizza is baked in an oven until the cheese is melted and bubbly. Pizza is known for its delicious flavors, combination of textures, and versatility, making it a beloved choice for meals, parties, and gatherings worldwide."
+       },
+    {
+      id: 6,
+      image: "../images/nonvegthali.jpg",
+      name: "Non-Veg Thali",
+      category: "dinner",
+      price: "180₹",
+      description:
+        "A non-vegetarian thali is a traditional Indian meal that includes a variety of dishes with non-vegetarian (meat-based) components. It typically consists of a large platter or plate with small bowls or compartments filled with different dishes. A non-vegetarian thali may include a variety of meat dishes like chicken, mutton, fish, or seafood, along with lentils (dal), curries, rice, bread (such as naan or roti), yogurt, pickles, and desserts. The specific dishes can vary based on regional preferences and culinary traditions. The non-vegetarian thali provides a balanced and flavorsome combination of meat-based dishes with accompanying sides, offering a satisfying and diverse dining experience."  
+      },
+    {
+      id: 7,
+      image: "../images/sweet.jpg",
+      name: "Sweets",
+      category: "dinner",
+      price: "60₹",
+      description:
+        "Sweet mithai refers to a wide variety of traditional Indian sweets and desserts. These treats are often made with ingredients like sugar, milk, ghee (clarified butter), nuts, and aromatic spices. They come in various shapes, sizes, and textures. Some popular examples of sweet mithai include:"
+       },
+    {
+      id: 8,
+      image: "../images/rajmarice.jpg",
+      name: "Rajma Rice",
+      category: "lunch",
+      price: "60₹",
+      description:
+        "Rajma rice is a popular North Indian dish that combines kidney beans (rajma) with rice to create a flavorful and satisfying meal. The kidney beans are cooked in a rich and aromatic tomato-based gravy, seasoned with a blend of spices like cumin, coriander, turmeric, and chili powder. The beans become soft and tender while absorbing the flavors of the spices. The rajma curry is typically served alongside steamed rice, providing a wholesome and balanced combination. Rajma rice is enjoyed for its hearty and comforting nature, making it a favorite choice for lunch or dinner in North Indian cuisine."
+       },
+    {
+      id: 9,
+      image: "../images/samosa.jpg",
+      name: "samaso",
+      category: "evening",
+      price: "10₹",
+      description:
+        "Samosa is a popular Indian snack known for its triangular shape and delicious filling. It consists of a crispy, deep-fried pastry shell made from a mixture of all-purpose flour, salt, and oil. The filling is typically made with spiced potatoes, peas, onions, and a variety of aromatic spices such as cumin, coriander, turmeric, and chili powder. Sometimes, samosas can also contain minced meat or lentils as an alternative filling. The pastry is folded and sealed around the filling, creating the iconic triangular shape. Samosas are often served hot and enjoyed with chutneys or sauces as a savory and flavorful snack. They are commonly found at Indian restaurants, street food stalls, and enjoyed at festive occasions."
+       },
+  ];
+  
+  export default Menu;
